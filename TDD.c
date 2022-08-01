90bb726368fd934da6158c3f00d6fae71ae414e8
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-01 21:31:23 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -16,21 +16,19 @@ int check_readings(int *array, int size)
 {
     int count=0;
     int i;
-    printf("Array has range");
+    
     for(i=0; i<size; i++)
     {
         if ((array[i+1]) == (array[i] + 1))
         {
-            count++;
-             printf("%d, ", array[i]);
-            
-        }
+            count++;          
+        }  
         else
         {
         }
         
     }
-    printf("and %d readings\n", count-1);
+    printf("Array has %d readings\n", count-1);
     
   return count;
 }
