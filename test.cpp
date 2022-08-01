3883072062c722c4ca20b2b2c2b09f100ba4930a
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-01 16:53:00 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,10 +3,17 @@
 #include "test/catch.hpp"
 
 
-TEST_CASE("checkAndAlert two") 
+TEST_CASE("Test case 1") 
 {
-
   int array[2] = {4,5};
   int size = sizeof(array);
   REQUIRE(check_readings(array,size) == 2);
 }
+
+TEST_CASE("Test case 1") 
+{
+  int array[3] = {4,5,6};
+  int size = sizeof(array);
+  REQUIRE(check_readings(array,size) == 3);
+}
+
