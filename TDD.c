27e42e35aee656b848e9a2ee182d735f7032bcad
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-01 15:33:41 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -1,29 +1,72 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
-char size(int cms) 
+#define ONE 1
+#define first_arr_idx_start 0
+void check_readings(int *array, int size);
+
+int main()
 {
-    char sizeName = '\0';
-    if(cms <= 38) 
-    {
-        sizeName = 'S';
-    } 
-    else if(cms > 38 && cms < 42) 
-    {
-        sizeName = 'M';
-    } 
-    else if(cms > 42) 
-    {
-        sizeName = 'L';
-    }
-    return sizeName;
-}
-
-int main() {
-    assert(size(37) == 'S');
-    assert(size(38) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    printf("All is well (maybe!)\n");
+    int array[9] = {1,2,3,4,5,10,11,12,13};
+    int size = sizeof(array);
+    check_readings(array,size);
     return 0;
 }
+
+void check_readings(int *array, int size)
+{
+    int i=0;
+    int j=0;
+    int second_arr_idx_start;
+    
+    int count_1=0;
+    int count_2=0;
+    
+    int second_array;
+    int print=0;
+    
+    printf("Range\n");
+    for(i=0; i<size; i++)
+    {
+        if(array[i] == (array[first_arr_idx_start] + i))
+        {
+            printf("%d," ,array[i]);
+            count_1 = count_1 + ONE;
+        }
+        else
+        {
+            second_arr_idx_start = i;
+            if(print == 0)
+            {
+                printf("\n");
+                print++;
+            }
+            
+            else
+            {
+                //do nothing
+            }
+            
+            for(second_array=i; second_array<size; second_array++)
+            {
+                if(array[second_array] == (array[second_arr_idx_start] + j))
+                {
+                    printf("%d," ,array[second_array]);
+                    count_2 = count_2 + ONE;
+                    j = j+ONE;
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+           
+        }
+        
+    }
+    printf("\n\nReadings\n");
+    printf("%d\n", count_1);
+    printf("%d\n", count_2);
+}
+
