73e43f678e4a6d1d214136d78b925fdd6a1284d7
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-01 16:35:52 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define ONE 1
+#define first_arr_idx_start 0
+int check_readings(int *array, int size);
+
+int main()
+{
+    int array[9] = {1,2,3,4,5,10,11,12,13};
+    int size = sizeof(array);
+    int done = check_readings(array,size);
+    return 0;
+}
+
+int check_readings(int *array, int size)
+{
+  return 2;
+}
+
 
