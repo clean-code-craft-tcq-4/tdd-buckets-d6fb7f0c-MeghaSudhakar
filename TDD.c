0e5c3e94b08716a708b11d721f57b313040f85fc
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 09:39:21 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include "TDD.h"
 
 int check_readings(int *array, int size);
 
@@ -12,25 +13,5 @@ int main()
     return 0;
 }
 
-int check_readings(int *array, int size)
-{
-    int count=0;
-    int i;
-    
-    for(i=0; i<size; i++)
-    {
-        if ((array[i+1]) == (array[i] + 1))
-        {
-            count++;          
-        }  
-        else
-        {
-        }
-        
-    }
-    printf("Array has %d readings\n", count-1);
-    
-  return count;
-}
 
 
