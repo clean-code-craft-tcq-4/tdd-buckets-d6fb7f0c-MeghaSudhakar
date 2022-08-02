30b2d4be6e7a84215d5a7f7b0b34eabdfe465677
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 09:43:44 +0530

    Update TDD.h

--- TDD.h
+++ TDD.h
@@ -3,23 +3,3 @@
 #include <stdlib.h>
 
 
-int check_readings(int *array, int size)
-{
-    int count=0;
-    int i;
-    
-    for(i=0; i<size; i++)
-    {
-        if ((array[i+1]) == (array[i] + 1))
-        {
-            count++;          
-        }  
-        else
-        {
-        }
-        
-    }
-    printf("Array has %d readings\n", count-1);
-    
-  return count;
-}
