b9896b217f7b7eaefadd5179749e1a9559f17056
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 10:14:37 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -11,18 +11,19 @@ TEST_CASE("Test case 1")
   int size = sizeof(array);
   REQUIRE(check_readings(array,size) == 2);
 }
-/*
+
 TEST_CASE("Test case 2") 
 {
   int array[3] = {4,5,6};
   int size = sizeof(array);
   REQUIRE(check_readings(array,size) == 3);
 }
-
+/*
 TEST_CASE("Test case 2") 
 {
   int array[4] = {4,5,6,9};
   int size = sizeof(array);
   REQUIRE(check_readings(array,size) == 3);
 }
+
 */
