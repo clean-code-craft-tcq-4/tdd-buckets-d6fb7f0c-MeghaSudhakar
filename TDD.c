083eac076319abe18f9a0f34cfc34b84a7ae4115
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 17:42:47 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -7,24 +7,23 @@ int check_readings(int *array, int size)
 {
     int count=0;
     int first,second,i;
+    int second_array[10];
     
     for(i=0; i<size; i++)
     {
         if ((array[i+1]) == (array[i] + 1))
         {
             count= count+1;  
-            first = array[i];
-            //printf(" %d - %d", array[i], array[i+1]);
+            
+            printf(" %d - %d", array[i], array[i+1]);
         }  
         else
         {
-            second = array[i-1];
-            //if(!count)
-           // printf("\n");
+          
         }
         
     }
-    printf("%d - %d\n", first, second);
+  
     printf("\nArray has %d readings\n", count+1);
     
   return count;
