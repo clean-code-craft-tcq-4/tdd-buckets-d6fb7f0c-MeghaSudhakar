e6e98ed74844a7ca81b37431abcac2583318eabb
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 10:13:11 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -5,13 +5,14 @@
 
 
 
-int main()
+/*void main()
 {
     int array[5] = {1,2,4,5,6};
     int size = sizeof(array);
     int done = check_readings(array,size);
     return 0;
-}
+}*/
+
 int check_readings(int *array, int size)
 {
     return 2;
