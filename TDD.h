76475badff1e84cf4142974c0b068e9bbda3c0dc
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-02 15:18:31 +0530

    Update TDD.h

--- TDD.h
+++ TDD.h
@@ -1,5 +1,5 @@
 
 int check_readings(int *array, int size);
-//int read(int *array, int size);
+int read(int *array, int size);
 
-//void print(int first, int end, int count);
+void print(int first, int end, int count);
