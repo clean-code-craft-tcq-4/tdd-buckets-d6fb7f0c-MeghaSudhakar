773686a5ac4139cd1ec87b35c990332127a8738e
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-04 19:08:05 +0530

    Update TDD.h

--- TDD.h
+++ TDD.h
@@ -1,2 +1,6 @@
 
 int check_readings(int *array, int size);
+void printArray(int array[], int size);
+void sort_current_values(int *array, int arraysize);
+void swap(int *xp, int *yp);
+void Find_range(int *array, int arraysize);
