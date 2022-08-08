4dea728bbd624f141e167ab4ae97841515b24f59
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-08 10:01:26 +0530

    Update TDD.h

--- TDD.h
+++ TDD.h
@@ -1,6 +1,3 @@
 
 int check_readings(int *array, int size);
-void printArray(int array[], int size);
-void sort_current_values(int *array, int arraysize);
-void swap(int *xp, int *yp);
-int check_readings(int *array, int size)
+
