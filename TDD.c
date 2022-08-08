14b8fd8ccc33bdde2541dbe289785eb97e9a16bb
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-08 11:11:15 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -3,11 +3,57 @@
 #include <stdlib.h>
 #include "TDD.h"
 
+void sort_array(int *array, int size)
+{
+    int i, j, min_idx; 
+  
+    // One by one move boundary of 
+    // unsorted subarray 
+    for (i = 0; i < arraysize-1; i++) 
+    { 
+        
+        // Find the minimum element in 
+        // unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < arraysize; j++) 
+        {
+             if (array[j] < array[min_idx]) 
+             {
+                min_idx = j; 
+             }
+        }
+        // Swap the found minimum element 
+        // with the first element 
+        swap(&array[min_idx], &array[i]); 
+    } 
+}
+
+//Swap function
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+
+//Function to print an array
+void printArray(int array[], int size) 
+{ 
+    int i; 
+    printf("The charging current value during the process of charging is\n");
+    for (i=0; i < size; i++) 
+    {
+        printf("%d ", array[i]);
+    }
+     
+    printf("\n");  
+    printf("\n"); 
+}
+
 int check_readings(int *array, int size)
 {
     int count=0;
     int i;
-    
     
     for(i=0; i<size; i++)
     {
