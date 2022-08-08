bbdfab41b947683c620bd378f2e9594c6b2d9578
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-08 10:01:42 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -3,67 +3,6 @@
 #include <stdlib.h>
 #include "TDD.h"
 
-
-
-
-int main()
-{
-    int i;
-    int array[10] = {1,3,3,5,4,10,11,12,14,15};
-    int arraysize = sizeof(array)/sizeof(array[0]);
-    sort_current_values(array, arraysize);
-    printArray(array, arraysize);
-    Find_range(array, arraysize);
-    return 0;
-}
-
-void sort_current_values(int *array, int arraysize)
-{
-    int i, j, min_idx; 
-  
-    // One by one move boundary of 
-    // unsorted subarray 
-    for (i = 0; i < arraysize-1; i++) 
-    { 
-        
-        // Find the minimum element in 
-        // unsorted array 
-        min_idx = i; 
-        for (j = i+1; j < arraysize; j++) 
-        {
-             if (array[j] < array[min_idx]) 
-             {
-                min_idx = j; 
-             }
-        }
-        // Swap the found minimum element 
-        // with the first element 
-        swap(&array[min_idx], &array[i]); 
-    } 
-}
-
-//Swap function
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
-
-//Function to print an array
-void printArray(int array[], int size) 
-{ 
-    int i; 
-    printf("The charging current value during the process of charging is\n");
-    for (i=0; i < size; i++) 
-    {
-        printf("%d ", array[i]);
-    }
-     
-    printf("\n");  
-    printf("\n"); 
-} 
-
 int check_readings(int *array, int size)
 {
     int count=0;
