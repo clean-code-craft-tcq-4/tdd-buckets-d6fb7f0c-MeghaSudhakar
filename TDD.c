04fd5fa3faf0c0c69cc422e7f91d180281e1acbb
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-08 11:11:57 +0530

    Update TDD.c

--- TDD.c
+++ TDD.c
@@ -52,26 +52,33 @@ void printArray(int array[], int size)
 
 int check_readings(int *array, int size)
 {
-    int count=0;
-    int i;
-    
     for(i=0; i<size; i++)
     {
         if ((array[i+1]) == (array[i] + 1))
         {
             count= count+1;  
-            
-            printf(" %d - %d", array[i], array[i+1]);
+            flag=1;
+            printf(" %d - %d", array[i], array[i]+1);
         }  
         else
         {
+            if(flag)
+            {
+                printf("\nArray has %d readings\n", count+1);
+                flag=0;
+                count=0;
+            }
+            
+            else
+            {
+                
+            }
+            
           
+            //printf("\n");
         }
         
     }
-  
-    printf("\nArray has %d readings\n", count+1);
-    
-  return count;
 }
+  
 
