1c1a6c19d2a004da332be90625a1b6d05dbb0301
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-28 21:45:33 +0530

    Update AD_converter.c

--- AD_converter.c
+++ AD_converter.c
@@ -3,4 +3,17 @@ AD_Converter(int value)
 {
   //If the A2D reports 1146, it is 10 Amps * 1146 / 4094 = 2.799 Amps, which is rounded-off to the nearest integer 3
   amps = 10 * value / 4094;
+  return amps;
+}
+
+converter_check(int amps)
+{
+  if(amps >= 10)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
 }
