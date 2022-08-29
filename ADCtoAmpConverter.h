10a8c30efb37dca31abdaca4f334679ed0f81594
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 13:03:22 +0530

    Update ADCtoAmpConverter.h

--- ADCtoAmpConverter.h
+++ ADCtoAmpConverter.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdbool.h>
 #include <string.h>
 #include <float.h>
@@ -13,5 +14,5 @@ typedef struct{
   int MaxValueReadByAdc;
 }AdcParameters;
 
-
+extern const AdcParameters AdcParameterStruct;
 int ConvertAdcToAmp(int ADCValue, int* CurrentInAmp);
