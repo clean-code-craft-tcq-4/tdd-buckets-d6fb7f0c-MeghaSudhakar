52812c85f1066fe0c8ca258ee9442e16074366b1
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 15:13:05 +0530

    Update test_converter.h

--- test_converter.h
+++ test_converter.h
@@ -9,3 +9,11 @@ static inline void validateAdcConverter_NoError(int AdcOutput, int ExpectedCurre
   REQUIRE(ConvertAdcToAmp(AdcOutput, &CurrentInAmp) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
   REQUIRE(CurrentInAmp == ExpectedCurrentInAmp);
 }
+
+static inline void validateAdcConverter_Error(int AdcOutput, int ExpectedCurrentInAmp)
+{
+  int CurrentInAmp = 0;
+  
+  REQUIRE(ConvertAdcToAmp(AdcOutput, &CurrentInAmp) == ADCToAmpConversionNotSucessful); /* function returns 1 indicating error */
+  REQUIRE(CurrentInAmp == ExpectedCurrentInAmp);
+}
