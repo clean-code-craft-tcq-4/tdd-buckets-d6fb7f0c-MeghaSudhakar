b9a5038af537a98292ebbf064f5ff96cd7e37da9
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 13:24:27 +0530

    Update test_ADCtoAmpConverter.cpp

--- test_ADCtoAmpConverter.cpp
+++ test_ADCtoAmpConverter.cpp
@@ -1,7 +1,7 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+
 #include "test_converter.h"
 #include <string.h>
 #include "ADCToAmpConverter.h"
