bf79f7bf0bad3e147081bb70e012a6fb49f32bd2
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 12:56:18 +0530

    Update test_ADCtoAmpConverter.cpp

--- test_ADCtoAmpConverter.cpp
+++ test_ADCtoAmpConverter.cpp
@@ -6,3 +6,18 @@
 #include <stdlib.h>
 #include "ADCtoAmpConverter.h"
 
+const AdcParameters AdcParameterStruct = {
+  0,      // MinCurrentSensedByAdc in Amp
+  10,     // MaxCurrentSensedByAdc in Amp
+  10,     // AdcConversionScale -> Since this is single direction, maxCurrentSensedByAdc is considered as scale
+  0,      // AdcConversionOffset
+  4094    // MaxValueReadByAdc
+};
+
+
+TEST_CASE("To check if ADC o/p 0 is read as 0A by the converter")
+{
+  int AdcOutput = 0;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
