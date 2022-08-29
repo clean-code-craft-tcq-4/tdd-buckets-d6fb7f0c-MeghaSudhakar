087c437bdb38f4de848f0b5e375005ac3dd4a9ff
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 12:56:23 +0530

    Update ADCtoAmpConverter.h

--- ADCtoAmpConverter.h
+++ ADCtoAmpConverter.h
@@ -1,1 +1,14 @@
-AD_Converter(int value);
+#include <stdbool.h>
+#include <string.h>
+#include <float.h>
+
+#define ADCToAmpConversionSucessful      0
+#define ADCToAmpConversionNotSucessful   1
+
+typedef struct{
+  int MinCurrentSensedByAdc;
+  int MaxCurrentSensedByAdc;
+  int AdcConversionScale;
+  int AdcConversionOffset;
+  int MaxValueReadByAdc;
+}AdcParameters;
