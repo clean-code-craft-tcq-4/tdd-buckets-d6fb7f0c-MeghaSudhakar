6a8abce4ce57ab1840028bd0c7a1d86d400804e7
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-08-29 14:47:01 +0530

    Update test_ADCtoAmpConverter.cpp

--- test_ADCtoAmpConverter.cpp
+++ test_ADCtoAmpConverter.cpp
@@ -23,3 +23,24 @@ TEST_CASE("To check if ADC o/p 0 is read as 0A by the converter")
   int expectedCurrentInAmp = 0;
   validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
 }
+
+TEST_CASE("To check if ADC o/p 4094 is read as 10A by the converter")
+{
+  int AdcOutput = 4094;
+  int expectedCurrentInAmp = 10;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
+
+TEST_CASE("To check if ADC o/p 4095 or greater is read as error by the converter")
+{
+  int AdcOutput = 4095;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_Error(AdcOutput, expectedCurrentInAmp);
+}
+
+TEST_CASE("To check if ADC o/p 1146 is read as 3A by the converter")
+{
+  int AdcOutput = 1146;
+  int expectedCurrentInAmp = 3;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
